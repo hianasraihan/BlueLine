025cfec398a533e263006f714da14aaa8c5f2fce
hianasraihan <hianasraihan@gmail.com>
2025-06-26 16:41:09 +0700

    perubahan baru

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -10,7 +10,10 @@ export default function Head() {
       <link rel="canonical" href="https://blue-line-pearl.vercel.app/" />
 
       {/* Google Site Verification */}
-      <meta name="google-site-verification" content="78deb95975de25" />
+      <meta
+        name="google-site-verification"
+        content="googleaa78deb95975de25"
+      />
 
       {/* Open Graph / Facebook */}
       <meta
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,6 @@ export const metadata = {
   title: 'Blue Line AutoCare - Profesional dan Terpercaya',
   description:
     'Profesional dalam Perawatan, Bersih dalam Setiap Detail. Blue Line AutoCare siap melayani kebutuhan detailing kendaraan Anda.',
-  other: {
-    'google-site-verification': '78deb95975de25', // ← Kode dari Google
-  },
 };
 
 export default function Page() {
