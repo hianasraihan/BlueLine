0fee3e4ecda7926029cdcdc64dca7fce8828f343
hianasraihan <hianasraihan@gmail.com>
2025-07-03 21:38:46 +0700

    new code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
 import HomePage from './components/HomePage';
 
-export const metadata = {
-  title: 'Blue Line AutoCare',
-  description:
-    'Profesional dalam Perawatan, Bersih dalam Setiap Detail. Blue Line AutoCare siap melayani kebutuhan detailing kendaraan Anda.',
-};
-
 export default function Page() {
   return <HomePage />;
 }
