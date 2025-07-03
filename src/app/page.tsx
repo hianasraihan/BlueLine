a093d02f7c1df0a23b14c8d7cde2094c903d74e3
hianasraihan <hianasraihan@gmail.com>
2025-07-03 21:29:36 +0700

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import HomePage from './components/HomePage';
 
 export const metadata = {
-  title: 'Blue Line AutoCare - Profesional dan Terpercaya',
+  title: 'Blue Line AutoCare',
   description:
     'Profesional dalam Perawatan, Bersih dalam Setiap Detail. Blue Line AutoCare siap melayani kebutuhan detailing kendaraan Anda.',
 };
