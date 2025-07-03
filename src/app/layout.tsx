a8a284bc40618e6a57eeb9b12294d608269a8259
hianasraihan <hianasraihan@gmail.com>
2025-07-03 21:55:09 +0700

    bismillah di acc google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,45 +1,9 @@
-import type { Metadata } from "next";
+// app/layout.tsx
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Blue Line AutoCare",
-  description:
-    "Profesional dalam Perawatan, Bersih dalam Setiap Detail. Blue Line AutoCare siap melayani kebutuhan detailing kendaraan Anda dengan harga terjangkau dan kualitas internasional.",
-  robots: "index, follow",
-  metadataBase: new URL("https://blue-line-pearl.vercel.app"),
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon.ico",
-    apple: "/blue.png",
-  },
-  openGraph: {
-    title: "Blue Line AutoCare",
-    description:
-      "Profesional dalam Perawatan, Bersih dalam Setiap Detail. Blue Line AutoCare siap melayani kebutuhan detailing kendaraan Anda dengan harga terjangkau dan kualitas internasional.",
-    url: "https://blue-line-pearl.vercel.app/",
-    siteName: "Blue Line AutoCare",
-    images: [
-      {
-        url: "https://blue-line-pearl.vercel.app/og-image.jpg", // FULL URL
-        width: 1200,
-        height: 630,
-        alt: "Blue Line AutoCare",
-      },
-    ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Blue Line AutoCare",
-    description:
-      "Profesional dalam Perawatan, Bersih dalam Setiap Detail. Blue Line AutoCare siap melayani kebutuhan detailing kendaraan Anda dengan harga terjangkau dan kualitas internasional.",
-    images: ["https://blue-line-pearl.vercel.app/og-image.jpg"], // FULL URL
-  },
-};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
